703d8bcd839fd50a4960bdb370378b1462f2956c
Pavel Martchenkov <49005549+pmartche@users.noreply.github.com>
2026-04-11 23:21:17 -0400

    Chore: ci update
    * dev included in ci pipeline
    
    * ci: rename workflow job
    
    * Chore/update ci
    
    * ci changes; enforce-dev-to-main ci
    
    * minor ci fix
    
    * chore/ci fix
    
    * chore/ci fix - run prisma migration
    
    * chore/ci fix - configure test db

--- api-gateway/src/app/app.module.ts
+++ api-gateway/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [PrismaModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
