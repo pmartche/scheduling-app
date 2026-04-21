60edb3e7acf37e147cf1c33f4b6d2293255db8f1
pmartche <pavel.martchenkov@gmail.com>
2026-04-20 20:58:59 -0400

    business-backend

--- api-gateway/src/business/service/business.service.ts
+++ api-gateway/src/business/service/business.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBusinessDto } from '../dto/create-business.dto';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AddLocationDto } from '../dto/add-location.dto';
+import { UpdateBusinessDto } from '../dto/update-business.dto';
 
 @Injectable()
 export class BusinessService {
@@ -12,5 +14,17 @@ export class BusinessService {
 
   createBusiness(business: CreateBusinessDto) {
     return this.prismaService.business.create({ data: business });
+  }
+
+  addLocation(location: AddLocationDto) {
+    return this.prismaService.location.create({ data: location });
+  }
+
+  updateBusiness(business: UpdateBusinessDto) {
+    const { id, ...data } = business;
+    return this.prismaService.business.update({
+      where: { id },
+      data,
+    });
   }
 }
