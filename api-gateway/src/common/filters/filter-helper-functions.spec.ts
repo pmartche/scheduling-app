8c89404780b2fd696460792b7e434fd35030bb2f
pmartche <pavel.martchenkov@gmail.com>
2026-05-03 11:38:06 -0400

    fixed tsconfig

--- api-gateway/src/common/filters/filter-helper-functions.spec.ts
+++ api-gateway/src/common/filters/filter-helper-functions.spec.ts
@@ -1,10 +1,16 @@
+import { HttpStatus } from '@nestjs/common';
 import { mapPrismaErrorToResponse } from './filter-helper-functions';
 import { PRISMA_ERROR_TEST_CASES } from './filter-helper-functions.test-data';
+import { PrismaDerivedError } from '@scheduling-app/shared-config';
 
 describe('mapPrismaErrorToResponse', () => {
   it.each(PRISMA_ERROR_TEST_CASES)(
     'maps prisma error %s to status %s and error type %s',
-    (prismaError, statusCode, errorType) =>
+    (
+      prismaError: string,
+      statusCode: HttpStatus,
+      errorType: PrismaDerivedError,
+    ) =>
       expect(mapPrismaErrorToResponse(prismaError)).toEqual({
         statusCode,
         errorType,
